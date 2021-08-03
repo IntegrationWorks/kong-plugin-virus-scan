76f54f6a86fd21fe137168bc5c27f6a4127c4025
Vaughn Butt <vb@integration.works>
2021-08-03 15:21:16 +1200

    Updated to new application name

--- build-kong-go-plugin.sh
+++ build-kong-go-plugin.sh
@@ -1,4 +1,4 @@
 go mod tidy
-go build -o avscanner-client cmd/kong-av-scanner-client/main.go
+go build -o avscanner-client cmd/kong-plugin-virus-scan/main.go
 
 docker -D build -t kong-go-avscanner-demo .
