356a585b02053d685becb4ca5df9c3c6574eecdf
Jonathan White <jonathan.white@integration.works>
2022-07-26 14:26:27 +1200

    Explicitly set GO Build architecture. Update Kong to 2.8.1

--- build-kong-go-plugin.sh
+++ build-kong-go-plugin.sh
@@ -1,4 +1,4 @@
 go mod tidy
-go build -o avscanner-client cmd/kong-plugin-virus-scan/main.go
+env GOOS=linux GOARCH=amd64 go build -o avscanner-client cmd/kong-plugin-virus-scan/main.go
 
 docker -D build -t kong-go-avscanner-demo .
